11838bf7b6ab45079886b7a93e67ae507f4e5b5a
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-03-02 22:29:28 -0500

    create base page layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,17 @@
 <main>
-	<div class='toto'>
-		{#if $user.authenticated}
-			<Textfield class='p-8' variant='outlined' bind:value={email} label='E-mail' />
-		{:else}
-			<AuthPage />
-		{/if}
-	</div>
+	{#if $user.authenticated}
+		<PageBase />
+	{:else}
+		<AuthPage />
+	{/if}
 </main>
 
 <script lang='ts'>
 	import '../node_modules/svelte-material-ui/bare.css';
 	import AuthPage from './auth/AuthPage.svelte';
-	import Textfield from '@smui/textfield';
 	import { Externals, Stores, storesKey } from './stores';
 	import { setContext } from 'svelte';
+	import PageBase from './components/PageBase.svelte';
 
 	export let externals: Externals;
 	let stores = new Stores(externals);
@@ -28,10 +26,5 @@
 <style>
     main {
         @apply flex justify-center items-center h-full;
-    }
-
-    .toto {
-        @apply w-full max-w-xl;
-        @apply h-full;
     }
 </style>
---
+++ src/components/PageBase.svelte
@@ -0,0 +1,33 @@
+<script></script>
+
+<div class='page'>
+	<div class='navbar'>
+		<p class='title'>FIT-BUDDY</p>
+		<ul>
+		</ul>
+	</div>
+	<div class='body'>
+		<slot />
+	</div>
+</div>
+
+<style>
+    .page {
+        @apply flex flex-col h-full w-full;
+    }
+
+    .navbar {
+        @apply basis-14;
+        @apply flex justify-between items-center;
+        @apply shadow;
+    }
+
+    .title {
+        @apply text-xl font-bold tracking-wider;
+        @apply ml-5;
+    }
+
+    .body {
+        @apply flex-grow;
+    }
+</style>
